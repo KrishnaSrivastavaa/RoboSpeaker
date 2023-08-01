6b6ed5b64ef328f948f50c5e729a0f021900e933
Krishna Srivastava <7824Krishna@gmail.com>
2023-08-01 22:04:54 +0530

    Adding main code for our robo speaker project

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import pyttsx3
+
+if __name__ == '__main__':
+    engine = pyttsx3.init()
+    print("Welcome to RoboSpeaker 0.1 Created by Krishna!!!")
+    while True:
+        x = input("Enter what you want me to speak")
+        if x == "q":
+            engine.say("bye bye friend")
+            engine.runAndWait()
+            
+            break
+        engine.say({x})
+        engine.runAndWait()
+    
